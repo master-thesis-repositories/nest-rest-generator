11aa154b1b4867099caed9f930c18efd5f41e891
Ingo Andelhofs <ingoandelhofs@gmail.com>
2022-11-08 12:15:03 +0100

    :sparkles: Able to parse nest.js files on Get requests with their name

--- packages/generator/src/main.ts
+++ packages/generator/src/main.ts
@@ -1,2 +1,43 @@
+import * as fs from "fs";
+import ts from "typescript";
 
-console.log("hello ts");
+
+const node = ts.createSourceFile(
+  "a.ts",
+  fs.readFileSync("../nest/src/app.controller.ts", "utf-8"),
+  ts.ScriptTarget.Latest,
+);
+
+node.forEachChild(child => {
+  if (ts.SyntaxKind[child.kind] === "ClassDeclaration") {
+    child.forEachChild(child => {
+
+      if (ts.SyntaxKind[child.kind] === "MethodDeclaration") {
+
+        let requestMethod = "";
+        let name = "";
+        child.forEachChild(child => {
+
+          if (ts.SyntaxKind[child.kind] === "Decorator") {
+            // const callExpression = child.getChildren()[0];
+            // const identifier = callExpression.getChildren()[0];
+
+            // @ts-ignore
+            console.log("request method:", child.expression.expression.escapedText);
+          }
+
+          if (ts.SyntaxKind[child.kind] === "Identifier") {
+            // @ts-ignore
+            console.log("name:", child.escapedText);
+          }
+
+
+        });
+
+      }
+    });
+  }
+});
+
+// console.log(node);
+
